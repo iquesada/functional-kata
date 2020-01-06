e942040b9ff2432066ba731328e363abebf79f5c
Iñaki Quesada <iquesada@piedresybarro.com>
2020-01-06 23:09:36 +0100

    Constructor as functional

--- app/deck.js
+++ app/deck.js
@@ -2,12 +2,12 @@ const R = require('rambda');
 
 class Deck {
   constructor() {
-    let suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-    let playingCards = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    const suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    const playingCards = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     this.cards = []
     for(let i = 0; i < suits.length; i++)
       for(let j = 0; j < playingCards.length; j++)
-        this.cards.push(playingCards[j] + " of " + suits[i])
+        this.cards = R.concat(this.cards, [playingCards[j] + " of " + suits[i]]);
   }
 
   getCards() {
